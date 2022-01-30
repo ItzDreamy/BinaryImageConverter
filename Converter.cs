e92760b45d0530576e3f763c5636bcc69f7c7edd
ItzDreamy <emptybrawl@gmail.com>
2022-01-30 15:16:57 +0200

    init

---
+++ Converter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace BinaryImageConverter
+{
+    public class Converter
+    {
+        public void ImageToBytes(string pathToImage)
+        {
+            Console.WriteLine("Converting started...");
+            string imageBytes = string.Join(" ", File.ReadAllBytes(pathToImage));
+            File.WriteAllText("bytes.txt", imageBytes);
+            
+            Console.WriteLine("Success!");
+        }
+
+        public void BytesToImage(string pathToBytes)
+        {
+            string[] bytesInString = File.ReadAllText(pathToBytes).Split(' ');
+            byte[] imageBytes = new byte[bytesInString.Length];
+            
+            for (int i = 0; i < imageBytes.Length; i++)
+                imageBytes[i] = Convert.ToByte(bytesInString[i]);
+
+            using (Bitmap bitmap = new Bitmap(new MemoryStream(imageBytes)))
+                bitmap.Save("image.png", ImageFormat.Png);
+            
+            Console.WriteLine("Success!");
+        }
+        
+        public void ImageToBinary(string pathToImage)
+        {
+            Console.WriteLine("Converting started...");
+            byte[] bytes = File.ReadAllBytes(pathToImage);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            foreach (var b in bytes)
+                stringBuilder.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+
+            string imageBinary = stringBuilder.ToString();
+            
+            File.WriteAllText("binary.txt", imageBinary);
+
+            Console.WriteLine("Success!");
+        }
+
+        public void BinaryToImage(string pathToBinary)
+        {
+            string binary = File.ReadAllText(pathToBinary);
+
+            byte[] bytes = new byte[binary.Length / 8];
+            
+            for(int i = 0; i < bytes.Length; ++i)
+                bytes[i] = Convert.ToByte(binary.Substring(8 * i, 8), 2);
+
+            using (Bitmap bitmap = new Bitmap(new MemoryStream(bytes)))
+                bitmap.Save("image.png", ImageFormat.Png);
+            
+            Console.WriteLine("Success!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BinaryImageConverter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Converter converter = new Converter();
+
+            while (true)
+            {
+                Console.WriteLine("0-exit; 1-convert image to bytes; 2-convert bytes to image; 3-convert image to binary; 4-convert binary to image:");
+                string path;
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        break;
+                    case "1":
+                        Console.Write("Enter path to picture: ");
+                        path = Console.ReadLine();
+                        
+                        converter.ImageToBytes(path);
+                        continue;
+                    case "2":
+                        Console.Write("Enter path to bytes: ");
+                        path = Console.ReadLine();
+                        
+                        converter.BytesToImage(path);
+                        continue;
+                    case "3":
+                        Console.Write("Enter path to picture: ");
+                        path = Console.ReadLine();
+                        
+                        converter.ImageToBinary(path);
+                        continue;
+                    case "4":
+                        Console.Write("Enter path to binary code: ");
+                        path = Console.ReadLine();
+                        
+                        converter.BinaryToImage(path);
+                        continue;
+                    default:
+                        Console.Write("Invalid operation! \n");
+                        continue;
+                }
+                break;
+            }
+        }
+    }
+}
